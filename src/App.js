8fa07ae1eda2405235a97d589685c6543369f3fb
Martin Miglio <marmig0404@gmail.com>
2019-09-13 02:54:12 -0400

    Added landing page

--- src/App.js
+++ src/App.js
@@ -7,17 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h>
+          Welcome to my page
+        </h>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            -Martin
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
