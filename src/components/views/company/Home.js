9b307c855e81508c6a0a9b0406c0991b1a9cc887
Martin Miglio <marmig0404@gmail.com>
2020-06-11 02:38:31 -0400

    Added react-helment to handle header changes for pages

--- src/components/views/company/Home.js
+++ src/components/views/company/Home.js
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
+import { Helmet } from 'react-helmet';
 import "./../../../stylesheets/CompanyHome.css";
 import SocialBar from "../common/SocialBar";
 import Landing from "../common/Landing";
 import logo from "../../assets/DDCLogo1.png";
 
-export default class CompanyHome extends Component{
-    render(){
-        const contactContent = [
-            {
-              contactType: "twitter",
-              contactLink: "https://twitter.com/digidevllc"
-            },
-            {
-              contactType: "envelope",
-              contactLink: "mailto:mail@digidev.lcc"
-            },
-          ];
-        return(
-            <div className="company-wrapper">
-                <Landing logo={logo} header="Come back soon!" signature="Check Twitter for updates or email" className="company-landing"/>
-                <SocialBar contentList={contactContent} className="social" color="#282c34"/>              
-            </div>
-        );
-    }
+export default class CompanyHome extends Component
+{
+  render()
+  {
+    const contactContent = [
+      {
+        contactType: "twitter",
+        contactLink: "https://twitter.com/digidevllc"
+      },
+      {
+        contactType: "envelope",
+        contactLink: "mailto:mail@digidev.lcc"
+      },
+    ];
+    return (
+      <div className="company-wrapper">
+        <Helmet>
+          <title>{"DigiDev"}</title>
+        </Helmet>
+        <Landing logo={logo} header="Come back soon!" signature="Check Twitter for updates or email" className="company-landing" />
+        <SocialBar contentList={contactContent} className="social" color="#282c34" />
+      </div>
+    );
+  }
 }
--- src/components/views/portfolio/Home.js
+++ src/components/views/portfolio/Home.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { Helmet } from 'react-helmet';
 import "./../../../stylesheets/PortfolioHome.css";
 import Landing from "../common/Landing";
 import SocialBar from "../common/SocialBar";
-import logo from "../../assets/PortfolioLogo.svg"
+import logo from "../../assets/PortfolioLogo.svg";
 /* 
 import CollapsibleList from "./CollapsibleList";
 import About from "./About";
@@ -13,68 +14,73 @@ import Experience from "./Experience";
 */
 
 
-export default class PortfolioHome extends Component{
-    render(){
-        const contactContent = [
-            {
-              contactType: "github",
-              contactLink: "https://github.com/marmig0404"
-            },
-            {
-              contactType: "envelope",
-              contactLink: "mailto:marmig0404@gmail.com"
-            },
-            {
-              contactType: "linkedin",
-              contactLink: "https://www.linkedin.com/in/martinmiglio"
-            },
-            {
-              contactType: "cube",
-              contactLink: "https://www.thingiverse.com/marmig0404/"
-            }
-          ];
-        
-          /*
-          const educationContent = [
-            {
-              imageSource: KULogo,
-              header: "Kettering University",
-              body: "KU Body"
-            },
-            {
-              imageSource: IAMLogo,
-              header: "International Academy of Macomb",
-              body: "IAM Body"
-            }
-          ];
-        
-          const aboutContent = {
-            //imageSource:ProfilePic,
-            header: "Martin Miglio",
-            body: "About Body"
-          };
-        
-          const collapsibleListContent = [{
-            trigger: "About",
-            content: <About contentList={aboutContent} />
-          },
-          {
-            trigger: "Education",
-            content: <Education contentList={educationContent} />
-          },
-          {
-            trigger: "Experience",
-            content: <Experience />
-          },
-          ];
-          */
-        
-          return (
-            <div className="portfolio-wrapper" >
-              <Landing className = "portfolio-landing" logo= {logo} header="Martin Miglio" signature="Welcome to my page" />
-              <SocialBar contentList={contactContent} className="social" />
-              {/*<CollapsibleList contentList={collapsibleListContent} className="list" />*/}
-            </div>
-          );
-    }
+export default class PortfolioHome extends Component
+{
+  render()
+  {
+    const contactContent = [
+      {
+        contactType: "github",
+        contactLink: "https://github.com/marmig0404"
+      },
+      {
+        contactType: "envelope",
+        contactLink: "mailto:marmig0404@gmail.com"
+      },
+      {
+        contactType: "linkedin",
+        contactLink: "https://www.linkedin.com/in/martinmiglio"
+      },
+      {
+        contactType: "cube",
+        contactLink: "https://www.thingiverse.com/marmig0404/"
+      }
+    ];
+
+    /*
+    const educationContent = [
+      {
+        imageSource: KULogo,
+        header: "Kettering University",
+        body: "KU Body"
+      },
+      {
+        imageSource: IAMLogo,
+        header: "International Academy of Macomb",
+        body: "IAM Body"
+      }
+    ];
+  
+    const aboutContent = {
+      //imageSource:ProfilePic,
+      header: "Martin Miglio",
+      body: "About Body"
+    };
+  
+    const collapsibleListContent = [{
+      trigger: "About",
+      content: <About contentList={aboutContent} />
+    },
+    {
+      trigger: "Education",
+      content: <Education contentList={educationContent} />
+    },
+    {
+      trigger: "Experience",
+      content: <Experience />
+    },
+    ];
+    */
+
+    return (
+      <div className="portfolio-wrapper" >
+        <Helmet>
+          <title>{"Martin Miglio"}</title>
+        </Helmet>
+        <Landing className="portfolio-landing" logo={logo} header="Martin Miglio" signature="Welcome to my page" />
+        <SocialBar contentList={contactContent} className="social" />
+        {/*<CollapsibleList contentList={collapsibleListContent} className="list" />*/}
+      </div>
+    );
+  }
 }
