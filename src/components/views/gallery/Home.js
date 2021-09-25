6bb4b95435188a5cb0e10803375acc1ed2c954bc
Martin Miglio <marmig0404@gmail.com>
2021-09-24 21:33:42 -0400

    Changed gallery to pull image from gphotos

--- src/components/views/gallery/Home.js
+++ src/components/views/gallery/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import Landing from "../common/Landing";
-import logo from "../../assets/temp_gallery.png";
+//import logo from "../../assets/temp_gallery.png";
 // import Carousel from 'react-instagram-carousel';
 import "./../../../stylesheets/GalleryHome.css";
 
@@ -25,7 +25,7 @@ export default class GalleryHome extends Component {
           <title>{"Miglio Sights"}</title>
         </Helmet>
         <Landing
-          logo={logo}
+          logo="https://lh3.googleusercontent.com/pw/AM-JKLWl0VLclBpsVzc1qoK2xVRaRAJ91S9MG6HQDPAHmToQosTMVC_uOGgY8CaK6JpRt9zdHJzeO--a5-qQDU41j6fQuzCed5jQZIriMNdXa8OT5xYzj9YRXlAJJTP_9WLpXhuWpIkZSAxn9Xa2HaJfFxiI2g=w1421-h947-no?authuser=0"
           header="Come back soon!"
           signature="Check Twitter for updates"
           className="gallery-landing"
