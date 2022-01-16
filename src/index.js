0384f132460cda2ede142fa56fa5d937e6aa5cda
Martin Miglio <marmig0404@gmail.com>
2022-01-16 03:19:40 -0500

    updated tp react-scripts 5.0.0

--- src/index.js
+++ src/index.js
@@ -2,20 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./stylesheets/index.css";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
 
 const app = (
-  <div>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
+  <React.StrictMode>
     <App className="app" />
-  </div>
+  </React.StrictMode>
 );
+
 ReactDOM.render(app, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
