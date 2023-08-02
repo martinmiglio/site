dbdf42e7506e2a72a57a6b64e86b9d6a69bd8683
Martin Miglio <marmig0404@gmail.com>
2023-08-01 21:56:06 -0400

    update page icon

--- src/app/icon/route.tsx
+++ src/app/icon/route.tsx
@@ -10,14 +10,16 @@ export async function GET() {
     (
       <div
         style={{ fontFamily: '"Golos Text Bold"' }}
-        tw="text-[#00A824] bg-[#E8E8E8] text-2xl w-full h-full flex items-center justify-center rounded-md"
+        tw="text-[#00A824] bg-[#E8E8E8] text-5xl w-full h-full flex items-center justify-center rounded-2xl"
       >
-        M
+        <div tw="flex">
+          M<div tw="text-xl flex mb-auto">2</div>
+        </div>
       </div>
     ),
     {
-      width: 32,
-      height: 32,
+      width: 64,
+      height: 64,
       fonts: [
         {
           name: "Golos Text Bold",
