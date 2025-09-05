c873c5e5a2b44423c7e1cf88c577abc4732df296
Martin Miglio <marmig0404@gmail.com>
2025-09-04 23:29:56 -0400

    update drawer styling

--- src/components/ui/drawer/DrawerContent.vue
+++ src/components/ui/drawer/DrawerContent.vue
@@ -13,7 +13,7 @@ const props = withDefaults(defineProps<DrawerContentProps>(), {
 
 <template>
   <DrawerPortal>
-    <DrawerOverlay class="fixed inset-0 z-50 bg-black/20 dark:bg-black/40" />
+    <DrawerOverlay class="fixed inset-0 z-50 bg-theme-800" />
     <DrawerContent
       :class="cn(
         'fixed z-50 flex flex-col',
@@ -21,12 +21,12 @@ const props = withDefaults(defineProps<DrawerContentProps>(), {
         'inset-x-0 bottom-0 h-[85%] rounded-t-[10px] border-t',
         // Desktop: right-side drawer  
         'md:right-0 md:left-auto md:top-0 md:bottom-0 md:h-full md:w-[500px] md:max-w-[50vw] md:rounded-none md:rounded-l-[10px] md:border-l md:border-t-0',
-        'bg-white dark:bg-slate-950 border-slate-200 dark:border-slate-800',
+        'bg-theme-50 border-theme-200',
         props.class
       )"
     >
       <!-- Mobile drag handle -->
-      <div class="mx-auto mt-4 h-2 w-[100px] rounded-full bg-slate-300 dark:bg-slate-700 md:hidden" />
+      <div class="mx-auto mt-4 h-2 w-[100px] rounded-full bg-theme-200 md:hidden" />
       <slot />
     </DrawerContent>
   </DrawerPortal>
