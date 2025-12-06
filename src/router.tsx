5e14dbd5605b191bfff9886c4866e52d7e38b3ae
Martin Miglio <marmig0404@gmail.com>
2025-12-05 20:50:25 -0500

    fix the tanstack router interface

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,10 @@
 // src/router.tsx
 
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
-export function createRouter() {
-  const router = createTanStackRouter({
+export function getRouter() {
+  const router = createRouter({
     routeTree,
     scrollRestoration: true
   })
@@ -14,6 +14,6 @@ export function createRouter() {
 
 declare module '@tanstack/react-router' {
   interface Register {
-    router: ReturnType<typeof createRouter>
+    router: ReturnType<typeof getRouter>
   }
 }
