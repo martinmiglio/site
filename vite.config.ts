fd614bc6b332ba31d00e96a751a4979a41795ef6
Martin Miglio <marmig0404@gmail.com>
2025-12-25 01:27:38 -0500

    chore: bump react + tss versions

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
+    nitro({
+      preset: 'aws-lambda',
+      awsLambda: { streaming: true }
+    }),
     tanstackStart({
       customViteReactPlugin: true,
-      target: 'aws-lambda',
       prerender: {
         enabled: true,
         autoSubfolderIndex: true,
